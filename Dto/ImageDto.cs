0c0429c7759e62ce7868cf7aff18cb9633fd8530
SandeepGurajala <121610086+sandy1017h@users.noreply.github.com>
2025-03-26 17:55:42 +0530

    imagedto

--- Dto/ImageDto.cs
+++ Dto/ImageDto.cs
@@ -5,12 +5,16 @@ namespace server.Dto
     public class ImageDtoRes
     {
         private string _imageUrl;
-        public int Id { get; set; }
-        public string ImageUrl {
-            get { return Path.Combine("image", _imageUrl) ; }
-            set { this._imageUrl = value; }
+
+        public ImageDtoRes(string imageUrl = "")
+        {
+            _imageUrl = imageUrl;
         }
 
-
+        public int Id { get; set; }
+        public string ImageUrl
+        {
+            get { return Path.Combine("image", _imageUrl ?? string.Empty); }
+        }
     }
 }
